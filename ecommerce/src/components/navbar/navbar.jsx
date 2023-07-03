1e558d089f0a03d590bcaa08a842284b79135aa6
kbrsrkl1 <kubranur216@gmail.com>
2023-07-03 13:11:03 +0300

    commit

---
+++ ecommerce/src/components/navbar/navbar.jsx
@@ -0,0 +1,17 @@
+import { HiMenuAlt4 } from "react-icons/Hi";
+
+function Navbar() {
+    return ( 
+        <>
+        <div className="header">
+            <div className="container">
+                <div className="menuSearch">
+                    <div></div>
+                </div>
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default Navbar;

c8c191b19b80b715a256a79928c6030be42e507b
kbrsrkl1 <kubranur216@gmail.com>
2023-07-05 01:57:23 +0300

    commit

--- ecommerce/src/components/navbar/MenuNavigation.jsx
+++ ecommerce/src/components/navbar/MenuNavigation.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
-
-function MenuNavigation() {
-    return (
-        <div>
-            
+//import React from 'react'
+import { BsXLg } from "react-icons/bs"
+function MenuNavigation({menuToggle, 
+    setMenuToggle }) {
+    return ( 
+    <div
+    className={`${menuToggle ?
+    "menuNavigation open" :
+    "menuNavigation"}`}>
+    <div className="menu">
+        <div className="minim">Minim
         </div>
-    )
+        <div className="menuX" onClick={(prev) => setMenuToggle(!prev)}>
+            <BsXLg />
+        </div>
+    </div>
+    </div>
+    );
 }
 
-export default MenuNavigation
+export default MenuNavigation;

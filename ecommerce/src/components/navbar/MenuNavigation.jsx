824a21fdeb28b95671ee8bed5507f6fd537babbb
kbrsrkl1 <kubranur216@gmail.com>
2023-07-05 17:46:55 +0300

    commit

--- ecommerce/src/components/navbar/MenuNavigation.jsx
+++ ecommerce/src/components/navbar/MenuNavigation.jsx
@@ -1,12 +1,17 @@
 //import React from 'react'
-import { BsXLg } from "react-icons/bs"
+import { useState } from "react";
+import { menuDatas } from "../../data/data"
+import { BsXLg, BsChevronDown } from "react-icons/bs"
 function MenuNavigation({menuToggle, 
     setMenuToggle }) {
+        const [activeData, setActiveData] = useState(null);
+        const handleClick = (id) => {
+            setActiveData( id === activeData ? null :  id)
+        }
     return ( 
     <div
     className={`${menuToggle ?
-    "menuNavigation open" :
-    "menuNavigation"}`}>
+    "menuNavigation open" :"menuNavigation"}`}>
     <div className="menu">
         <div className="minim">Minim
         </div>
@@ -14,6 +19,36 @@ function MenuNavigation({menuToggle,
             <BsXLg />
         </div>
     </div>
+    <div className="menuDates">
+        {menuDatas.map((menuData, id) => {
+            return(
+                <div className="dataGroup"
+                key = {menuData.id} onClick={() => handleClick(id)} >
+                <div className="dataGroupName">
+                    <div>
+                        {menuData.dataGroupName}
+                        </div>
+                        <BsChevronDown  className={`${id === activeData ? "rotate180" : "rotate0"}`} />
+                </div>
+
+                {id === activeData && (
+                    <div className="dataItems">
+                        {menuData.dataItems.map
+                        ((dataItem, index) => (
+                            <a href="" key={index}>
+                                {dataItem}
+                            </a>
+                        )
+                        )}
+                    </div>
+                )}
+                </div>
+            )
+        })}
+    </div>
+
+    <div className="callUs">Call Us 09883271929</div>
+    <div className="openHours"> Operating Hour 9:00am to 7:30pm</div>
     </div>
     );
 }

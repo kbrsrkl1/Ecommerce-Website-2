61f2f1f783abd5d9f359f2a31c24857c4086cf90
kbrsrkl1 <kubranur216@gmail.com>
2023-07-07 23:04:23 +0300

    commit

--- ecommerce/src/components/container/PopularProducts.jsx
+++ ecommerce/src/components/container/PopularProducts.jsx
@@ -1,5 +1,25 @@
+import { products } from "../../data/data"
+import Products from "./Products";
+
 export default function PopularProducts(){
     return(
-        <section className="container"></section>
-    )
+        <section className="container min_100vh">
+            <div className="popular_container">
+                <div className="title">
+                    Popular <span>products</span>
+                </div>
+                <div className="nextAndPrev">
+                    <div>Prev</div>
+                    <span> | </span>
+                    <div>Next</div>
+                </div>
+            </div>
+            <div className="product_container">
+                {products.map(( product) => { 
+                    return <Products key = {product.id} {...product}
+                    />;
+                })}
+            </div>
+        </section>
+    );
 }

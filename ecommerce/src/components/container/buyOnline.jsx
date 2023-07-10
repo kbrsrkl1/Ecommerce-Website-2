09b1749a2a44c7819062a732cf7114e959936ed3
kbrsrkl1 <kubranur216@gmail.com>
2023-07-10 23:59:16 +0300

    COMMİT

--- ecommerce/src/components/container/buyOnline.jsx
+++ ecommerce/src/components/container/buyOnline.jsx
@@ -1,4 +1,10 @@
-export default function buyOnline(){
-    return <section className="container"></section>
-    
+import { BuyOnlineDatas } from "../../data/data";
+export default function BuyOnline() {
+    return <section className="container">
+        <div className="title">
+            When You <span>BUY ONLİNE</span>
+        </div>
+        <div className="BUY"></div>
+    </section>
+
 }
--- ecommerce/src/components/index.js
+++ ecommerce/src/components/index.js
@@ -2,6 +2,6 @@ import Navbar from "./navbar/navbar";
 import Home from "./container/home";
 import PopularProducts from "./container/PopularProducts"
 import Shopping from "./container/Shopping";
-import buyOnline from "./container/buyOnline";
-export { Navbar, Home , PopularProducts, Shopping, buyOnline};
+import BuyOnline from "./container/BuyOnline";
+export { Navbar, Home , PopularProducts, Shopping, BuyOnline};
 

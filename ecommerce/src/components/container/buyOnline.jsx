9ce94008511b43ba21f837128159730a8c463300
kbrsrkl1 <kubranur216@gmail.com>
2023-07-11 00:03:29 +0300

    commit

--- ecommerce/src/components/container/buyOnline.jsx
+++ ecommerce/src/components/container/buyOnline.jsx
@@ -1,10 +1,19 @@
-import { BuyOnlineDatas } from "../../data/data";
+import { BuyOnlineDatas, buyOnlineDatas } from "../../data/data";
 export default function BuyOnline() {
     return <section className="container">
         <div className="title">
             When You <span>BUY ONLİNE</span>
         </div>
-        <div className="BUY"></div>
+        <div className="buyOnline">
+            {buyOnlineDatas.map(data => (
+                <div className="data" key = {data.id}>
+                    <div className="data_title">
+                        {data.title}
+                    </div>
+                    <p>{data.text}</p>
+                </div>
+            ))}
+        </div>
     </section>
 
 }
